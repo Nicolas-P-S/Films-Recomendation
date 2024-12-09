cfb5c6490cadd21ce379c2ff8f32fb292fff3cdf
Nicolas <nicolasevil7@gmail.com>
2024-12-09 20:50:56 -0300

    add login/register/database

--- login.py
+++ login.py
@@ -1,4 +1,30 @@
 import os
+import sqlite3
+
+def save_user(usuario, senha):
+    conn = sqlite3.connect("login.db")
+    cursor = conn.cursor()
+    
+    cursor.execute("create table if not exists usuarios(id integer primary key autoincrement, nome text not null, senha text not null)")
+    cursor.execute("insert into usuarios (nome, senha) values (?, ?)", (usuario, senha)) #uso de placeholders(?) para evitar sql injection
+    conn.commit()
+    conn.close()
+
+def search_user(usuario):
+    try:
+        conn = sqlite3.connect("login.db")
+        cursor = conn.cursor()
+        cursor.execute("select nome, senha from usuarios where nome = ?", (usuario,))
+        retorno = cursor.fetchone()
+        if retorno:
+            return retorno  #tupla (usuario, senha)
+        else:
+            return None, None
+    except sqlite3.Error as erro:
+        print(f"Erro no banco de dados: {erro}")
+        return None, None
+
+
 
 def register():
     usuario = ""
@@ -18,20 +44,62 @@ def register():
             input("ERRO: senha deve ter entre 4 e 20 caracteres!")
         os.system("cls")
 
-    print("Registro criado com sucesso!","\nusuario:", usuario,"\nsenha:", senha)
+    print("Usuario criado com sucesso!","\nusuario:", usuario,"\nsenha:", senha)
     input()
-    os.system("cls")
+    save_user(usuario, senha)
     return usuario, senha
 
-def menu():
-    print("----------BEM VINDO ao bla bla bla----------")
-    escolha=input("\nEscolha a opção:\nRegister-1\nLogin-2\n\nin: ")
+def login():
+    if not os.path.exists("login.db"):
+        input("ERRO: NÃO EXISTEM USUARIOS CADASTRADOS NA DATABASE!")
+        return None, None
+    
+    while True:
+        os.system("cls")
+        print("Login\n")
+        usuario = input("user: ")
+        usuario_search, senha_search = search_user(usuario)
 
-    match int(escolha):
+        if not usuario_search:
+            input("Usuario invalido! Pressione Enter para tentar novamente.")
+        else:
+            break;
+
+    while True:
+        os.system("cls")
+        print("Login\n")
+        senha = input("password: ")
+        if senha == senha_search:
+            os.system("cls")
+            print(f"Bem-vindo de volta, {usuario}!")
+            return usuario, senha
+        else:
+            input("Senha incorreta! Pressione Enter para tentar novamente.")
+
+def menu():
+    os.system("cls")
+    print("----------BEM VINDO ao bla bla bla----------")
+    try:
+        escolha = int(input("\nEscolha a opção:\nRegister-1\nLogin-2\n\nin: "))
+    except ValueError:
+        input("Entrada inválida! Pressione Enter para tentar novamente.")
+        return menu()
+
+    match escolha:
         case 1:
             os.system("cls")
             usuario, senha = register()
-            return usuario, senha
+            if usuario is None or senha is None:
+                return menu()
+            else:
+                return usuario, senha
         case 2:
             os.system("cls")
-            print("l")
+            usuario, senha = login()
+            if usuario is None or senha is None:
+                return menu()
+            else:
+                return usuario, senha
+        case _:
+            input("Opção inválida! Pressione Enter para tentar novamente.")
+            return menu()
